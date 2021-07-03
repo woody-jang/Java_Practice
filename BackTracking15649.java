1dd3824ac32b69ae144e2c448ff31c364cfecdc1
장세호(Jang Seho) <82800087+woody-jang@users.noreply.github.com>
2021-07-03 12:40:35 +0900

    Backtracking algorithm.
    백준 15649문제
    처음 접한 유형이라 많이 고민하다가
    재귀를 이용해서 문제 해결

---
+++ BackTracking15649.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다.
+중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ */
+
+/*
+예제 입력 1 
+3 1
+예제 출력 1 
+1
+2
+3
+
+예제 입력 2 
+4 2
+예제 출력 2 
+1 2
+1 3
+1 4
+2 1
+2 3
+2 4
+3 1
+3 2
+3 4
+4 1
+4 2
+4 3
+
+예제 입력 3 
+4 4
+예제 출력 3 
+1 2 3 4
+1 2 4 3
+1 3 2 4
+1 3 4 2
+1 4 2 3
+1 4 3 2
+2 1 3 4
+2 1 4 3
+2 3 1 4
+2 3 4 1
+2 4 1 3
+2 4 3 1
+3 1 2 4
+3 1 4 2
+3 2 1 4
+3 2 4 1
+3 4 1 2
+3 4 2 1
+4 1 2 3
+4 1 3 2
+4 2 1 3
+4 2 3 1
+4 3 1 2
+4 3 2 1
+ */
+
+// for문을 두번째로 입력한 숫자만큼 실행해야되서 고민함
+// 재귀밖에 생각이 안나서 재귀를 써봄
+// 백준의 재귀 문제들을 풀고난후 재귀를 쓰는데 많이 도움이됨
+// 숫자들끼리 겹치면 안되기때문에 list에 재귀 이전의 숫자들을 담음
+// list를 출력할때도 활용함
+
+public class BackTracking15649 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuffer sb = new StringBuffer();
+
+		int n = Integer.parseInt(st.nextToken()); // 숫자 범위 받음
+		int m = Integer.parseInt(st.nextToken()); // 반복 횟수 받음
+
+		List<Integer> postNumbers = new ArrayList<>(); // 재귀시 앞의 숫자들을 담을 리스트 생성
+		
+		getNumbers(n, m, m, postNumbers, sb); // 숫자범위, 반복횟수, 출력조건용 갯수, 앞 숫자들 리스트를 넣어서 넘겨줌
+
+		System.out.println(sb);
+	}
+
+	private static void getNumbers(int n, int m, int cnt, List<Integer> postNumbers, StringBuffer sb) {
+		if (m > 0) { // 반복횟수가 0보다 크면 반복
+			for (int i = 1; i <= n; i++) {
+				if (postNumbers.contains(i)) // 이전 재귀에서 추가한 숫자와 겹치면 continue
+					continue;
+				postNumbers.add(i); // 없다면 list에 현재 숫자 추가
+				getNumbers(n, m - 1, cnt, postNumbers, sb); // 재귀 시작
+				if (postNumbers.size() == cnt) { // 만약 리스트의 숫자갯수와 반복횟수가 일치한다면
+					for (int j = 0; j < postNumbers.size(); j++) { // stringbuffer에 숫자 추가
+						sb.append(postNumbers.get(j) + " ");
+					}
+					sb.append("\n"); // 횟수만큼 추가후 개행
+				}
+				postNumbers.remove(postNumbers.size() - 1); // 다시 반복을 돌기때문에 마지막에 추가한 숫자 제거
+			}
+		}
+	}
+}
